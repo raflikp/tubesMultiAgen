79ccc3f7c1f95fbdd90263cddb862649f2417a6d
Muhammad Zidan Ramadhan <147800862+rama1581@users.noreply.github.com>
2025-04-15 18:42:31 +0700

    nambah

--- main.py
+++ main.py
@@ -4,12 +4,48 @@ from q_learning import q_learning
 from utils import generate_direction_map, print_direction_map, save_q_table
 
 # Konfigurasi MAP
-width = 4
-height = 4
-start = (0, 0)
-goal = (3, 3)
-obstacles = [(1, 1), (2, 1)]
 
+# Bentuk 4 X 4
+# width = 4
+# height = 4
+# start = (0, 0)
+# goal = (3, 3)
+
+# obstacles = [
+#     (1, 1),  
+#     (2, 2)
+# ]
+
+# Bentuk 5 X 5
+# width = 5
+# height = 5
+# start = (0, 0)
+# goal = (4, 4)
+
+# obstacles = [
+#     (4, 0),  
+#     (1, 1),
+#     (4, 1),
+#     (2, 2), 
+#     (0, 3),
+#     (4, 3),
+#     (2, 4)
+# ]
+
+# Bentuk 8 X 8
+width = 8
+height = 8
+start = (0, 1)
+goal = (7, 7)
+
+obstacles = [
+    (3, 2),  
+    (5, 3),
+    (3, 4),
+    (1, 5), (2, 5), (6, 5),
+    (1, 6), (4, 6),(6, 6),
+    (3, 7)
+]
 env = GridWorld(width, height, start, goal, obstacles)
 
 # Parameter SARSA
@@ -21,7 +57,7 @@ epsilon = 0.1
 # Latih Algoritma
 Q, waktu, actions = sarsa(env, episodes, alpha, gamma, epsilon)
 
-# Tampilkan hasil
+# Tampilkan hasilS
 print(f"Waktu pelatihan: {waktu:.2f} detik")
 print("Jumlah aksi (↑ ↓ ← →):", actions)
 
